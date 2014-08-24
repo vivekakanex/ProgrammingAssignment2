a2d908c1e3d146b96484419882865fe2ca3a8509
vivekakanex <vivek.a.radhakrishnan@gmail.com>
2014-08-24 05:04:18 -0700

    Updating the changes made to the methods to perform the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setminverse <- function(minverse) m <<- minverse
+    getminverse <- function() m
+    list(set = set, get = get,
+         setminverse = setminverse,
+         getminverse = getminverse)
+  
 }
 
 
@@ -12,4 +24,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getminverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setminverse(m)
+  m
 }
+
+
+# > newMat<-makeCacheMatrix(MyMatrix)
+# > cacheSolve(newMat)
+# [,1]         [,2]         [,3]
+# [1,]  0.10281871 -0.014413837 -0.003096306
+# [2,] -0.05573350  0.004697843  0.018293117
+# [3,]  0.01313261  0.019965834 -0.005587586
+# > cacheSolve(newMat)
+# getting cached data
+# [,1]         [,2]         [,3]
+# [1,]  0.10281871 -0.014413837 -0.003096306
+# [2,] -0.05573350  0.004697843  0.018293117
+# [3,]  0.01313261  0.019965834 -0.005587586
